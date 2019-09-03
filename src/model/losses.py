a1fcf35b7633d192d2706a533731cb8c457ac230
tung i <tungi@tungde-MacBook-Pro.local>
2019-09-03 20:10:38 +0800

    normal bdloss

--- src/model/losses.py
+++ src/model/losses.py
@@ -25,3 +25,43 @@ class DiceLoss(nn.Module):
         union = (output ** 2).sum(reduced_dims) + (target ** 2).sum(reduced_dims)
         score = intersection / (union + 1e-10)
         return 1 - score.mean()
+
+
+class NormalBDiceLoss(nn.Module):
+    """The BDice loss.
+    """
+    def __init__(self):
+        super().__init__()
+
+    def forward(self, output, target):
+        """
+        Args:
+            output (torch.Tensor) (N, C, *): The model output.
+            target (torch.LongTensor) (N, 1, *): The data target.
+        Returns:
+            loss (torch.Tensor) (0): The dice loss.
+        """
+        # Define the class of tumor
+        class_t = output.size(1) - 1
+        batch_size = target.size(0)
+        # Get the one-hot encoding of the ground truth label.
+        target = torch.clamp(target, min=0, max=2)
+        target = torch.zeros_like(output).scatter_(1, target, 1) # (N, 1, *) -> (N, C, *)
+
+        reduced_dims = list(range(2, output.dim()))
+
+        w_b = torch.ones_like(target.sum(reduced_dims)) # (N, C)
+        w_b = w_b / batch_size
+        partial_volume = target.sum(reduced_dims)[:, class_t]  # (N, 1)
+        partial_volume = partial_volume**0.3333
+        total_volume = partial_volume.sum()
+        w_b[:, class_t] = (partial_volume / (total_volume + 1e-10))
+
+        intersection = (output*target).sum(reduced_dims) # (N, C)
+        union = (output**2).sum(reduced_dims) + (target**2).sum(reduced_dims)
+
+        _score = 2.0 * w_b * intersection / (union + 1e-10)
+        _score = _score.mean()
+        _loss = 1.0 - _score
+
+        return _loss
